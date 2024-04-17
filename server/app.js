8e7f62e0624109f411260a0415b9fceede698a6f
Prason <itsprason@gmail.com>
2024-04-17 23:26:14 +0545

    Enable CORS

--- server/app.js
+++ server/app.js
@@ -9,6 +9,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+// Middleware to enable CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specified HTTP methods
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allow specified headers
+    next();
+});
+
+
 // Routes 
 app.use('/auth', authRoutes);
 app.use('/aqi', aqiRoutes);
