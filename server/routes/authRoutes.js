cb395aee775b3481b47e2036edd4bf93ca21ffc9
Prason <itsprason@gmail.com>
2024-04-17 12:13:00 +0545

    Add JWT middleware for secure  authorization

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { registerUser, loginUser } from '../controllers/authController.js';
+import { authenticateJWT } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/register', registerUser);
